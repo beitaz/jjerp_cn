4ad8d259fb9ec0dc7e48aab76ee275bef996ccdd
beitaz <beitaz@icloud.com>
2017-10-17 16:38:26 +0800

    修改个人页面

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def model_name_helper
+    model_name = controller_name.singularize # 获取 Model 名称
+    # model = model_name.classify.constantize # 构造 Model 类
+    t('activerecord.models.' + model_name)
+  end
+
+  def action_name_helper
+    t(action_name)
+  end
 end
